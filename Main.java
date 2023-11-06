7a36e885485fa27c07dd1f308be53d5e76f88d54
senteujoshua <117025098+senteujoshua@users.noreply.github.com>
2023-11-06 17:25:09 +0300

    first commit

--- Main.java
+++ Main.java
@@ -1,8 +1,37 @@
 
+
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+      Multiplication product = new Multiplication();
+      Scanner scanner= new Scanner(System.in);
 
-        System.out.printf("Hello World!");
+      System.out.println( "Enter  your first operand");
+     double num1= scanner.nextDouble();
+      System.out.println( "Enter  your operator");
+      String operator= scanner.nextLine();
+      System.out.println( "Enter  your second operand");
+      double num2= scanner.nextDouble();
+      switch (operator){
+        case "*" :
+          double result= product.multiply(12,12);
+          System.out.println(result);
+          break;
+        default:
+          System.out.println("invalid operator");
+          break;
+
+
+
+
+
+      }
+
+
+
+
 
     }
 }
---
+++ Multiplication.java
@@ -0,0 +1,6 @@
+class Multiplication {
+  public double multiply(double num1, double num2) {
+    return num1 * num2;
+  }
+
+}
