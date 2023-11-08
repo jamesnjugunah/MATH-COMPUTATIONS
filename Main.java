105fe28bf077270d328e8444fb78dbc32452f983
james_njugunah <145548430+jamesnjugunah@users.noreply.github.com>
2023-11-08 17:39:24 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,42 +1,19 @@
-import java.util.Scanner;
+class perimeter{
+    int w;
+    int l;
+    int perimeter;
 
+    public void perimeter(int l,int w ) {
+        int i = l + w;
+        perimeter=2*i;
+       System.out.println("Perimeter of the rectangle is: "+perimeter);
+    }
+
+}
 public class Main {
-  public static void main(String[] args) {
-    Multiplication product = new Multiplication();
-    Addition sum =new Addition();
-    Scanner scanner = new Scanner(System.in);
-    
-
-    System.out.println("Welcome to our Circle Area Calculator!");
-    System.out.print("Enter the radius of the circle: ");
-
-    double radius = scanner.nextDouble();
-
-    double area = CircleArea.calculateArea(radius);
-
-    System.out.println("The area of the circle with radius " + radius + " is: " + area);
-
-    System.out.println("Enter  your first operand");
-    double num1 = scanner.nextDouble();
-    System.out.println("Enter  your operator");
-    String operator = scanner.nextLine();
-    System.out.println("Enter  your second operand");
-    double num2 = scanner.nextDouble();
-    switch (operator) {
-      case "*":
-        double result = product.multiply(num1,num2);
-        System.out.println(result);
-        break;
-      case "+":
-        double result = sum.add(num1,num2);
-        System.out.println(result);
-        break;
-      default:
-        System.out.println("invalid operator");
-        break;
-
+    public static void main(String[] args) {
+        perimeter c=new perimeter();
+        c.perimeter(16, 10);
 
     }
-    scanner.close();
-  }
 }
