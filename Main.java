e81337c32e26334c520ad209fcb10eac50056de3
Senteu <117025098+senteujoshua@users.noreply.github.com>
2023-11-08 15:30:17 +0300

    Add files via upload
    switched operator

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Multiplication product = new Multiplication();
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("Welcome to our Circle Area Calculator!");
@@ -13,6 +14,23 @@ public class Main {
 
     System.out.println("The area of the circle with radius " + radius + " is: " + area);
 
+    System.out.println("Enter  your first operand");
+    double num1 = scanner.nextDouble();
+    System.out.println("Enter  your operator");
+    String operator = scanner.nextLine();
+    System.out.println("Enter  your second operand");
+    double num2 = scanner.nextDouble();
+    switch (operator) {
+      case "*":
+        double result = product.multiply(12, 12);
+        System.out.println(result);
+        break;
+      default:
+        System.out.println("invalid operator");
+        break;
+
+
+    }
     scanner.close();
   }
 }
